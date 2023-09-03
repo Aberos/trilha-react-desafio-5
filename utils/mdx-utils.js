ef0cd5ceb87e285c25b308fb8a7507833871ba26
Mario Junior <mario_souza.junior@hotmail.com>
2023-09-03 18:15:20 -0300

    Implementada a logica do metodo getPostBySlug em mdx-utils.js

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -11,9 +11,9 @@ export const getPosts = async () => {
 }
 
 export const getPostBySlug = async (id) => {
-
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
-
+    const { data } = await api.get(`/posts?id=eq.${id}`);
+    if (data?.length > 0) {
+        return data[0];
+    }
     return {}
 }
